5c9be61ac6735bcf421237c4ae2ae1e759f12d14
Eudes Serpa <serpagame@gmail.com>
2022-05-02 01:00:06 -0500

    Find flight sections and recommendations complete with functional carousel

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
+// Logic for recommedations card carousel
+const carousel = document.querySelector(
+  ".recommendations__list-cards--carousel"
+);
+const cards = document.querySelectorAll(".card");
+const btnNext = document.querySelector(".carousel-button--next");
+const btnPrev = document.querySelector(".carousel-button--prev");
+
+const cardWidth = cards[0].offsetWidth;
+let currentCard = 0;
+
+const changeCard = () => {
+  carousel.scrollLeft = currentCard * cardWidth;
+};
+
+carousel.addEventListener("scroll", () => {
+  // For mobile scrolling
+  currentCard = Math.round(carousel.scrollLeft / cardWidth);
+
+  if (currentCard === 0) {
+    btnPrev.disabled = true;
+  } else if (currentCard === cards.length - 1) {
+    btnNext.disabled = true;
+  } else {
+    btnPrev.disabled = false;
+    btnNext.disabled = false;
+  }
+});
+
+btnNext.addEventListener("click", () => {
+  currentCard++;
+  changeCard();
+
+  if (currentCard < cards.length - 1) {
+    btnPrev.disabled = false;
+  } else {
+    btnNext.disabled = true;
+  }
+});
+
+btnPrev.addEventListener("click", () => {
+  currentCard--;
+  changeCard();
+
+  if (currentCard > 0) {
+    btnNext.disabled = false;
+  } else {
+    btnPrev.disabled = true;
+  }
+});
+
+// --- Logic for menu icon in mobile ---
 const menuMobile = document.querySelector(".header__menu--icon-mobile");
 
 menuMobile.addEventListener("click", () => {
